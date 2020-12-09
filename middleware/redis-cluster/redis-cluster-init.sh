5d04f35347fcd704c37a854648672da2fd20870d
niushaohan <shaohan.niu@dfgroup.pro>
2020-12-09 14:17:08 +0800

    Redis cluster: add node

--- middleware/redis-cluster/redis-cluster-init.sh
+++ middleware/redis-cluster/redis-cluster-init.sh
@@ -5,6 +5,10 @@ dir=$(pwd)
 # Redis config
 daemonize="yes"
 
+# Features
+init=true
+newNode=""
+
 ### Function
 function generateConfFile() {
     echo """
@@ -26,14 +30,48 @@ function generateConfFile() {
     """ > $1/redis.conf
 }
 
-# Init redis cluster, we plan to build a redis cluster, include 3 master and 3 slave.
-mkdir -p 7000/data 7001/data 7002/data 7003/data 7004/data 7005/data
+function usage() {
+    echo -e "redis-cluster provides the following features: \n"
+    echo -e "Options"
+    echo -e "    -i, --init                 执行初始化集群的操作"
+    echo -e "    --new-node <node port>     创建新节点"
+    echo -e "    -h, --help                 查看帮助"
+}
 
-generateConfFile 7000
-generateConfFile 7001
+while [ "$1" != "" ]; do
+    case $1 in
+        -i | --init       )         init=true
+                                    ;;
+        --new-node        )         shift
+                                    init=false
+                                    newNode="$1"
+                                    ;;
+        -h | --help       )         usage
+                                    exit 0
+                                    ;;
+        *                 )         usage
+                                    exit 1
+    esac
+    shift
+done
 
-generateConfFile 7002
-generateConfFile 7003
+if $init ; then
+    # Init redis cluster, we plan to build a redis cluster, include 3 master and 3 slave.
+    mkdir -p 7000/data 7001/data 7002/data 7003/data 7004/data 7005/data
 
-generateConfFile 7004
-generateConfFile 7005
+    generateConfFile 7000
+    generateConfFile 7001
+
+    generateConfFile 7002
+    generateConfFile 7003
+
+    generateConfFile 7004
+    generateConfFile 7005
+fi
+
+if [ "$newNode" != "" ] 
+then 
+    mkdir -p $newNode/data
+    generateConfFile $newNode
+    echo -e "New node $newNode created."
+fi
--- middleware/redis-cluster/redis-cluster-start.sh
+++ middleware/redis-cluster/redis-cluster-start.sh
@@ -1,13 +1,55 @@
 #!/bin/bash
 
-redis-server 7000/redis.conf
-redis-server 7001/redis.conf
+startInitRedisServer=false
+createRedisCluster=false
+addNode=false
+startServerPort=""
 
-redis-server 7002/redis.conf
-redis-server 7003/redis.conf
+function usage() {
+    echo -e "redis-cluster-start provides the following features: \n"
+    echo -e "Options"
+    echo -e "    --start-init-server           启动初始的 redis server"
+    echo -e "    --start-server <server port>  启动指定的 redis server"
+    echo -e "    --create-cluster              创建集群"
+    echo -e "    -h, --help                    查看帮助"
+}
 
-redis-server 7004/redis.conf
-redis-server 7005/redis.conf
+while [ "$1" != "" ]; do
+    case $1 in
+        --start-init-server    )    startInitRedisServer=true
+                                    ;;
+        --start-server       )      shift
+                                    startServerPort="$1"
+                                    ;;
+        --create-cluster       )    
+                                    createRedisCluster=true
+                                    ;;
+        -h | --help       )         usage
+                                    exit 0
+                                    ;;
+        *                 )         usage
+                                    exit 1
+    esac
+    shift
+done
 
-redis-cli --cluster create 127.0.0.1:7000 127.0.0.1:7001 \
-  127.0.0.1:7002 127.0.0.1:7003 127.0.0.1:7004 127.0.0.1:7005 --cluster-replicas 1
+if $startInitRedisServer ; then
+    redis-server 7000/redis.conf
+    redis-server 7001/redis.conf
+
+    redis-server 7002/redis.conf
+    redis-server 7003/redis.conf
+
+    redis-server 7004/redis.conf
+    redis-server 7005/redis.conf
+fi
+
+if $createRedisCluster ; then
+    redis-cli --cluster create 127.0.0.1:7000 127.0.0.1:7001 \
+      127.0.0.1:7002 127.0.0.1:7003 127.0.0.1:7004 127.0.0.1:7005 --cluster-replicas 1
+fi
+
+if [ "$startServerPort" != "" ] 
+then
+    redis-server $startServerPort/redis.conf
+fi
